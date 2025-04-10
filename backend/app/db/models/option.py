38db1d76d80115c2884ecebb80a28bb23baeaac0
shiraishimuneyuki <muneyuki.0901ms@icloud.com>
2025-04-10 13:28:29 +0900

    修正

--- backend/app/db/models/option.py
+++ backend/app/db/models/option.py
@@ -5,10 +5,14 @@ from app.db.base import Base
 
 class OptionORM(Base):
     __tablename__ = "options"
+
     id = Column(Integer, primary_key=True)
     label = Column(String, nullable=False)
     rating = Column(Float, default=1500)
+    times_compared = Column(Integer, default=0, nullable=False)
+
     theme_id = Column(Integer, ForeignKey("themes.id"))
     theme = relationship("ThemeORM", back_populates="options")
-    times_compared = Column(Integer, default=0, nullable=False)
-    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
