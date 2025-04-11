201c44acaba246769abae628e6de32762a5c03f0
shiraishimuneyuki <muneyuki.0901ms@icloud.com>
2025-04-11 14:23:53 +0900

    レーティング計算用関数追加

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from app.api.endpoints import themes
+from app.api.endpoints import themes, vote
 
 api_router = APIRouter()
-# ここで prefix="/themes" を付けるので最終的に /themes となる
 api_router.include_router(themes.router, prefix="/themes", tags=["themes"])
+api_router.include_router(vote.router, tags=["vote"])
